862076d0d5178c632650eba61bca06809f0af659
Kajotko <jotkarolina@gmail.com>
2019-01-08 18:28:00 +0000

    renaming

--- collect.bat
+++ collect.bat
@@ -1,3 +1,3 @@
 @echo off
-start C:\Users\Karolina\Anaconda22\python "C:\Users\Karolina\OneDrive - Imperial College London\Year 4\Sensing\Coursework\code\siot.py" & start C:\Users\Karolina\Anaconda22\python "C:\Users\Karolina\OneDrive - Imperial College London\Year 4\Sensing\Coursework\code\stream_starter.py"
+start C:\Users\Karolina\Anaconda22\python "C:\Users\Karolina\OneDrive - Imperial College London\Year 4\Sensing\Coursework\code\weather_collect.py" & start C:\Users\Karolina\Anaconda22\python "C:\Users\Karolina\OneDrive - Imperial College London\Year 4\Sensing\Coursework\code\stream_starter.py"
 pause
